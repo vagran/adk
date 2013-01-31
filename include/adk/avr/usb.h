f0d2f8155fb3406e1bc577a0d0ae368ff40380d1
Artyom Lebedev <artjoms.lebedevs@accenture.com>
2013-01-31 17:11:02 +0200

    Fixed copyright years.

--- include/adk/avr/usb.h
+++ include/adk/avr/usb.h
@@ -1,7 +1,7 @@
 /* /ADK/include/adk/avr/usb.h
  *
  * This file is a part of 'ADK' project.
- * Copyright (c) 2012, Artyom Lebedev <artyom.lebedev@gmail.com>
+ * Copyright (c) 2012-2013, Artyom Lebedev <artyom.lebedev@gmail.com>
  * All rights reserved.
  * See COPYING file for copyright details.
  */
