9634298c423d85b2a8104cd6f8449af5570a7856
Artyom Lebedev <artyom.lebedev@gmail.com>
2015-03-11 08:04:09 +0200

    Copyright year updated.

--- tools/usb_capture_analyzer.py
+++ tools/usb_capture_analyzer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is a part of ADK library.
-# Copyright (c) 2012-2014, Artyom Lebedev <artyom.lebedev@gmail.com>
+# Copyright (c) 2012-2015, Artyom Lebedev <artyom.lebedev@gmail.com>
 # All rights reserved.
 # See COPYING file for copyright details.
 
